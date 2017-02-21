109a65966740e7b7d382b22073f11fa2d63013b2
Melody Lam <melody077077@yahoo.com.hk>
2017-02-21 16:19:35 +0800

    Install OneSignal

---
+++ app/src/main/java/com/html5killer/MyApplication.java
@@ -0,0 +1,19 @@
+package com.html5killer;
+
+import android.app.Application;
+
+import com.onesignal.OneSignal;
+
+/**
+ * Created by melody on 2/21/2017.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        OneSignal.startInit(this).init();
+
+    }
+
+}
