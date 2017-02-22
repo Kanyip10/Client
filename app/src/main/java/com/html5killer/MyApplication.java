2af0c445dffe91371645afa12c9f1f5cc8b7c22e
Melody Lam <melody077077@yahoo.com.hk>
2017-02-22 21:53:25 +0800

    update

--- app/src/main/java/com/html5killer/MyApplication.java
+++ app/src/main/java/com/html5killer/MyApplication.java
@@ -5,7 +5,7 @@ import android.app.Application;
 import com.onesignal.OneSignal;
 
 /**
- * Created by melody on 2/21/2017.
+ * Created by Melody Lam on 2/21/2017.
  */
 
 public class MyApplication extends Application {
